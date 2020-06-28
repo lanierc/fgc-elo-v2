4c9f9f6b1a197202a1dffe291b92c4679736999f
Corey Lanier <iam@coreylanier.com>
2020-06-27 22:39:50 -0400

    added token to header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { getToken } from '../../services/tokenService';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  token: String;
+
+  constructor() {
+    this.token = getToken();
+  }
 
   ngOnInit(): void {}
 }
